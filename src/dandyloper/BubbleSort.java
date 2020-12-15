094581fe7c6d77464245c5be57bb0b367de6bbb4
jadekang <jkang.se@gmail.com>
2020-12-15 03:22:03 -0800

    Built BubbleSort algorithm in Java.

--- src/dandyloper/BubbleSort.java
+++ src/dandyloper/BubbleSort.java
@@ -1,9 +1,27 @@
 package dandyloper;
 
 public class BubbleSort  {
-    public static void bubbleSort(String[] args) {
+    public void runBubbleSort (int[] intArray) {
 
+        for(int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            for (int i=0; i < lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[i+1]) {
+                    swap(intArray, i, i + 1);
+                }
+            }
+        }
+
+        for(int item : intArray) {
+            System.out.println(item);
+        }
     }
     // TODO: Write a swap method
-
+    public static void swap (int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
--- src/dandyloper/Main.java
+++ src/dandyloper/Main.java
@@ -4,18 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int[] intArray = new int[7];
+//        int[] intArray = new int[7];
+//
+//        intArray[0] = 20;
+//        intArray[1] = 35;
+//        intArray[2] = -15;
+//        intArray[3] = 7;
+//        intArray[4] = 55;
+//        intArray[5] = 1;
+//        intArray[6] = -22;
+//
+//        for (int i : intArray) {
+//            System.out.println(i);
+//        }
+        // This is where I run my bubble sort method
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
-        intArray[0] = 20;
-        intArray[1] = 35;
-        intArray[2] = -15;
-        intArray[3] = 7;
-        intArray[4] = 55;
-        intArray[5] = 1;
-        intArray[6] = -22;
-
-        for (int i : intArray) {
-            System.out.println(i);
-        }
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.runBubbleSort(intArray);
     }
+
 }
