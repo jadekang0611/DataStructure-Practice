ee0b4e5f44af08e56af8b6922355f019289ef9f6
jadekang <jkang.se@gmail.com>
2020-12-17 04:17:34 -0800

    Built the swap method for the SelectionSort.

--- src/dandyloper/SelectionSort.java
+++ src/dandyloper/SelectionSort.java
@@ -1,4 +1,17 @@
 package dandyloper;
 
 public class SelectionSort {
+    public void runSelectionSort(int[] intArray) {
+
+    }
+
+    // Build the swap method
+    public static void swap (int[] array, int i, int j) {
+        if(i == j) {
+            return;
+        }
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
