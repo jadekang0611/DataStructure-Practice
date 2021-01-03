8916e3f69e58f6c6198b9d550aceed925afc93e5
jadekang <jkang.se@gmail.com>
2021-01-03 14:10:48 -0800

    Implemented the ShellSort algorithm.

--- src/dandyloper/ShellSort.java
+++ src/dandyloper/ShellSort.java
@@ -1,4 +1,28 @@
 package dandyloper;
 
 public class ShellSort {
+    public void runShellSort (int[] intArray){
+        for (int gap = intArray.length / 2; gap > 0; gap /= 2) {
+
+            for (int i = gap; i < intArray.length; i++) {
+                int newElement = intArray[i];
+
+                int j = i;
+
+                while (j <= gap && intArray[j - gap] > newElement) {
+                    intArray[j] = intArray[j-gap];
+                    j -= gap;
+                }
+
+                intArray[j] = newElement;
+
+            }
+
+        }
+        System.out.println("\n-------Shell Sort--------");
+        for(int item : intArray){
+            System.out.println(item);
+        }
+        System.out.println("-----------------------------");
+    }
 }
